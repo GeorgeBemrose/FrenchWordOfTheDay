3eff6d1a7e7fa80c0a3406ec6d5310417a1cdd25
GeorgeBemrose <GeorgeBemrose@gmail.com>
2025-01-06 14:50:18 +0000

    Initial Commit

---
+++ FrenchWordOfTheDay/FrenchWordOfTheDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrenchWordOfTheDayApp.swift
+//  FrenchWordOfTheDay
+//
+//  Created by George Bemrose on 06/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct FrenchWordOfTheDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
